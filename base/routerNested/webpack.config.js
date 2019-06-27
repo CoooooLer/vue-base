ace7bfffa2c5966d0606d048b49fbe49c8d50fee
CoooooLer <13939@qq.com>
2019-06-27 13:44:20 +0800

    route webpack

---
+++ base/routerNested/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/vm.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.min.js'
+    }
+}
